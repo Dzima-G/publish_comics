c75ece4a516444bc02cfb13bcbbc0a9f6a68d922
Dzima-G <dgukalin@gmail.com>
2023-08-01 18:25:40 +0300

    combined functions download comic

--- main.py
+++ main.py
@@ -6,45 +6,33 @@ from urllib.parse import urlparse
 import sys
 
 
-def get_random_comic_number():
+def download_random_comic():
     first_comic_number = 1
     response = requests.get('https://xkcd.com/info.0.json')
     response.raise_for_status()
     last_comic_number = int(response.json().get('num'))
     random_comic_number = randint(first_comic_number, last_comic_number)
-    return random_comic_number
 
-
-def get_comic(comic_number):
-    comic_url = f'https://xkcd.com/{comic_number}/info.0.json'
+    comic_url = f'https://xkcd.com/{random_comic_number}/info.0.json'
     response = requests.get(comic_url)
     response.raise_for_status()
     response_json = response.json()
-    comic = {
-        'image_url': response_json.get('img'),
-        'alt': response_json.get('alt'),
-    }
-    return comic
+    comic_image_url = response_json.get('img')
+    comic_alt = response_json.get('alt')
 
-
-def get_comic_image(api_url):
-    response = requests.get(api_url)
+    response = requests.get(comic_image_url)
     response.raise_for_status()
-    return response
+    comic_image = response
 
-
-def get_image_name(image_url, comic_number):
-    path = urlparse(image_url).path
+    path = urlparse(comic_image_url).path
     extension = os.path.splitext(path)[1]
     name = os.path.splitext(path)[0].split('/')[-1]
-    image_name = f'comic_{comic_number}_{name}.{extension}'
-    return image_name
+    image_name = f'comic_{random_comic_number}_{name}.{extension}'
 
-
-def save_comic_image(file_image, image_name):
     file_path = os.path.join(image_name)
     with open(file_path, 'wb') as file:
-        file.write(file_image.content)
+        file.write(comic_image.content)
+    return image_name, comic_alt
 
 
 def get_photo_upload_url(access_token, api_version, group_id):
@@ -107,17 +95,7 @@ if __name__ == "__main__":
     vk_api_version = os.environ['VK_API_VERSION']
 
     try:
-        comic_number = get_random_comic_number()
-    except requests.exceptions.HTTPError as error:
-        print(error, file=sys.stderr)
-
-    comic = get_comic(comic_number)
-    comic_alt = comic.get('alt')
-    comic_image = get_comic_image(comic.get('image_url'))
-    image_name = get_image_name(comic.get('image_url'), comic_number)
-    save_comic_image(comic_image, image_name)
-
-    try:
+        image_name, comic_alt = download_random_comic()
         photo_upload_url = get_photo_upload_url(vk_access_token, vk_api_version, vk_group_id)
         server, photo, vk_hash = upload_comic_server(image_name, photo_upload_url)
         owner_id, media_id = save_comic_album(vk_access_token, vk_api_version, vk_group_id, server, photo, vk_hash)
